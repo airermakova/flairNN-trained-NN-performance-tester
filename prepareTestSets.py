14b73264453b4195ade2b4a4eed11d61ec6e0159
airermakova <56444081+airermakova@users.noreply.github.com>
2022-06-03 12:26:13 +0200

    Add files via upload

--- prepareTestSets.py
+++ prepareTestSets.py
@@ -1,8 +1,15 @@
+from flair.data import Corpus
+from flair.datasets import ColumnCorpus
+from flair.embeddings import WordEmbeddings, StackedEmbeddings
+from flair.models import SequenceTagger
+from flair.trainers import ModelTrainer
+from flair.data import Sentence
 import re
 import nltk
 import numpy
 import random
-import sys
+import codecs
+from threading import Thread
 from langdetect import detect
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
@@ -19,9 +26,25 @@ from nltk.stem.snowball import SnowballStemmer
 from nltk.corpus import words
 from nltk.corpus import conll2000, conll2002
 
-stemmer = SnowballStemmer("english")
+trainSet = []
+users = []
+globalUsers = []
+globalCount = []
 
-#TO GET USERS FROM TEXT FILE
+#TO GET PHRASES
+def getPhrasesFromFile(fileName):
+    phrases= []    
+    tokenized_phrases = []
+    try:
+       with codecs.open(fileName, 'r', "utf-8") as file:
+           phrases = file.read().split(".")
+       return phrases
+    except:
+      print("Error in reading " + fileName)
+      exit()
+
+#TO MARK USERS AS WE HAVE DONE FOR TRAIN SET
+
 def getDataFromFile(fileName):
     users = []        
     try:
@@ -36,46 +59,51 @@ def getDataFromFile(fileName):
           print(users[len(users)-1])
       exit()
 
-#TO GET TEXTS FROM TEXT FILE
-def getWordsFromFile(fileName):
-    phrases= []    
-    tokenized_phrases = []
-    try:
-       with open(fileName, 'r') as file:
-           phrases = file.read().split(".")
-       return phrases
-    except:
-      print("Error in reading " + fileName)
-      exit()
 
-#MARK USERS USING LIST OF PREDEFINED USERS
+
+#TO GET DATA FROM TEST FILE AND USERS FILE
+
+trainSet = getPhrasesFromFile("testData.txt")
+print("Taken phrases - ")
+print(len(trainSet))
+users = getDataFromFile("usersList.txt")
+print("Taken users - ")
+print(len(users))
+
+
 
 def checkMarkedArrayPresence(phrases, users):
-    checkUsers = False
-    pr = []
-    wr = []
-    finArr = []
     onlyUsers = []
-    phrasesArr=[]
-    cancelId = True
     for phrase in phrases:
         nltk_tags = pos_tag(word_tokenize(phrase))  
         iob_tagged = tree2conlltags(nltk_tags) 
         userFound = False
         for user in users:
-            ind = phrase.find(user)            
+            ind = phrase.find(user)
+            us = user.split(" ")
             if ind>0:
-               us = user.split(" ")
                iob_tagged = markUser(iob_tagged, us)
         for iob in iob_tagged:
             if iob[2]=="B":
                 onlyUsers.append(iob_tagged)          
     return onlyUsers
-                
+         
+
+       
 def markUser(phrase,user):
     finTuple = []
     users = []
     found = False
+    print(user)
+    exists = user in globalUsers
+    if exists == False:
+        globalUsers.append(user)
+        globalCount.append(0)
+    else:
+        ind = globalUsers.index(user)
+        globalCount[ind] = globalCount[ind]+1
+    print("Length of all detected users " + str(len(globalCount)))
+    
     if len(user)>1:
        nonFirst = False
        cnt = 0
@@ -99,10 +127,8 @@ def markUser(phrase,user):
                    ls = list(phrase[ucnt-cnt+i])
                    ls[2]="I"
                    finTuple[ucnt-cnt+i]=tuple(ls)
-               cnt = 0           
-       print(finTuple)
+               cnt = 0
     else:
-        print(user)
         for i in range(0, len(phrase)):
            finTuple.append(phrase[i])         
            if phrase[i][0] == user[0] and phrase[i][1][0]=="N":  
@@ -127,19 +153,16 @@ def markUser(phrase,user):
                    finTuple[cnt+1] = (tuple(ls))
     return finTuple
  
-
-#WRITE TRAIN FILE VALIDATION FILE AND TEST FILE
-
 def writeResultFile(trainSet):
-    t = open("train.txt", "a")
-    v = open("val.txt", "a")
-    te = open("test.txt", "a")
+    t = open("train2.txt", "a")
+    v = open("val2.txt", "a")
+    te = open("test2.txt", "a")
     for arr in trainSet:
         for s in arr:
             for st in s:
                 t.write((st + " ").encode("utf-8").decode("utf-8"))
-            t.write("\n".encode("utf-8").decode("utf-8"))            
-        t.write("\n\n\n".encode("utf-8").decode("utf-8")) 
+            t.write("\n")            
+        t.write("\n\n\n") 
     t = 0
     for arr in trainSet:
         t = t+1
@@ -148,9 +171,8 @@ def writeResultFile(trainSet):
             for s in arr:
                 for st in s:
                     v.write((st + " ").encode("utf-8").decode())
-                v.write("\n".encode("utf-8").decode())
-            v.write("\n\n\n".encode("utf-8").decode()) 
-            #f.write(' '.join(str(s) for s in arr) + "\n\n\n")
+                v.write("\n")
+            v.write("\n\n\n") 
     t = 0
     for arr in trainSet:
         t = t+1
@@ -159,14 +181,27 @@ def writeResultFile(trainSet):
             for s in arr:
                 for st in s:
                     te.write((st + " ").encode("utf-8").decode())
-                te.write("\n".encode("utf-8").decode())
-            te.write("\n\n\n".encode("utf-8").decode()) 
+                te.write("\n")
+            te.write("\n\n\n") 
      
          
-def writeUsersFile(finalArray):
-    f = open("onlyUsers.txt", "a")
-    for arr in finalArray:
-        f.write(' '.join(str(s) for s in arr) + "\n")  
+def writeUsersFile():
+    if len(globalUsers) != len(globalCount):
+        return
+    usersFile.write("\n\n\n\n")
+    for i in range(0, len(globalCount)):
+        for gl in range(0, len(globalUsers[i])):
+            usersFile.write(str(globalUsers[i][gl]) + " ") 
+        usersFile.write(": ")
+        usersFile.write(str(globalCount[i]))
+        usersFile.write("\n")  
+
+
+#TO PREPARE USERS RECOGNITION FOR MULTITHREADING
+def writeUsers(trainSet, users):
+    fin = checkMarkedArrayPresence(trainSet, users)
+    writeResultFile(fin)
+    writeUsersFile()
 
 #MAIN SCRIPTS
 
@@ -174,17 +209,27 @@ users = []
 users = getDataFromFile("usersList.txt")
 #print(users)
 
-trainSet = []
-valSet = []
-testSet = []
-fileName = str(sys.argv[1])
-print(fileName)
-trainSet = getWordsFromFile(fileName)
-
 
 f = open("onlyUsers.txt", "w")
 f.close()
 
-fin = checkMarkedArrayPresence(trainSet, users)
-writeResultFile(fin)
+rep = 0
+i=0
+phrases = []
+usersFile = open("onlyDetectedUsersMulti.txt", "a")
+for ph in trainSet:
+    i=i+1
+    phrases.append(ph)
+    if i>=100:
+        thread = Thread(target=writeUsers, args=(list(phrases), users))
+        thread.start()
+        i=0
+        phrases = []
+        rep = rep + 1
+
+
+
+
+
+
 
